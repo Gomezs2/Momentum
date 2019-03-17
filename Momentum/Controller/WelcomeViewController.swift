00165bdc93dd5d2e1df2f5ba65f3334ef9279c62
Sergio Gomez <Sergiogomez112@gmail.com>
2019-03-17 13:47:39 -0400

    Added file structure (#1)
    * added file structure
    
    * Integrated CocoaPods & Developed Welcome View

---
+++ Momentum/Controller/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Momentum
+//
+//  Created by Sergio Gomez on 3/13/19.
+//  Copyright © 2019 Triceratops. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
