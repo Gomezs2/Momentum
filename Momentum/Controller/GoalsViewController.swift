da1c788894e08c9d6501499da7d425a4a54459a1
Sergio Gomez <sergiogomez@uilleann-pipes-80.dynamic2.rpi.edu>
2019-03-23 20:13:02 -0400

    Completed Create Goal Functionality

--- Momentum/Controller/GoalsViewController.swift
+++ Momentum/Controller/GoalsViewController.swift
@@ -17,8 +17,6 @@ class GoalsViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var goalsTableView: UITableView!
     
     
-//    private var data = [Goal]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,14 +25,6 @@ class GoalsViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         // Register .xib file
         goalsTableView.register(UINib(nibName: "GoalCell", bundle: nil), forCellReuseIdentifier: "customGoalCell")
-        
-
-//        self.title = "GoalsTableView"
-//
-//        data = [Goal.init(title : "Title", progress : 50),
-//                Goal.init(title : "Go to Gym", progress : 30),
-//                Goal.init(title : "Momentum Project", progress: 10)]
-        
     }
     
     // Init cells
@@ -42,9 +32,6 @@ class GoalsViewController: UIViewController, UITableViewDataSource, UITableViewD
         let cell = tableView.dequeueReusableCell(withIdentifier: "customGoalCell", for: indexPath) as! GoalsTableViewCell
         cell.title.text = "G1"
         return cell
-        
-//        cell.commonInit(name: data[indexPath.item].title)
-//        return cell
     }
     
 //    Declare number of rows
