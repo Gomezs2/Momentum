67257740547c483a679132e1f691bd5e24ede29e
LingCheng3273 <LingCheng3273@gmail.com>
2019-03-25 17:40:49 -0400

    Added WelcomeBack view

--- Momentum/Controller/LoginViewController.swift
+++ Momentum/Controller/LoginViewController.swift
@@ -34,7 +34,7 @@ class LoginViewController: UIViewController {
             (user, error) in
             if error == nil {
                 SVProgressHUD.dismiss()
-                self.performSegue(withIdentifier: "goToGoals", sender: self)
+                self.performSegue(withIdentifier: "goWelcomeBack", sender: self)
             }
             else{
                 SVProgressHUD.dismiss()
