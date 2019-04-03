2f455fe185c8b86839c413db097cdcab4a04c16f
HOYOUNG98 <kevin@Kevins-iMac.local>
2019-04-02 17:31:31 -0700

    added tableView for milestones

---
+++ Momentum/View/CustomCell/MilestonesTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MilestonesTableViewCell.swift
+//  Momentum
+//
+//  Created by Kevin Kim on 4/2/19.
+//  Copyright © 2019 Triceratops. All rights reserved.
+//
+
+import UIKit
+
+class MilestonesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var endDate: UILabel!
+    
+    
+}
